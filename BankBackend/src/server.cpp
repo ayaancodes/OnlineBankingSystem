b2ee0ca73344e1fb95ed8dcfaf447c777c9f9d3d
Ayaan Munshi <ayaanmunshi456@gmail.com>
2025-03-30 00:02:55 -0400

    Minor server.cpp fix

--- BankBackend/src/server.cpp
+++ BankBackend/src/server.cpp
@@ -9,8 +9,10 @@
 using tcp = boost::asio::ip::tcp;
 namespace http = boost::beast::http;
 
-void do_session(tcp::socket socket) {
-    try {
+void do_session(tcp::socket socket)
+{
+    try
+    {
         boost::beast::flat_buffer buffer;
         http::request<http::string_body> req;
         http::read(socket, buffer, req);
@@ -18,27 +20,34 @@ void do_session(tcp::socket socket) {
         http::response<http::string_body> res;
         handle_request(req, res);
         http::write(socket, res);
-    } catch (const std::exception& e) {
+    }
+    catch (const std::exception &e)
+    {
         std::cerr << "❌ Session error: " << e.what() << std::endl;
     }
 }
 
-int main() {
-    try {
+int main()
+{
+    try
+    {
         std::cerr << "🔥 MAIN() REACHED 🔥\n";
         boost::asio::io_context ioc{1};
         tcp::acceptor acceptor{ioc, {tcp::v4(), 8080}};
 
         std::cout << "🚀 Server running on http://localhost:8080\n";
 
-        for (;;) {
+        for (;;)
+        {
             tcp::socket socket{ioc};
             acceptor.accept(socket);
-std::thread([sock = std::move(socket)]() mutable {
-    do_session(std::move(sock));
-}).detach();
+            std::thread([sock = std::move(socket)]() mutable
+                        { do_session(std::move(sock)); })
+                .detach();
         }
-    } catch (const std::exception& e) {
+    }
+    catch (const std::exception &e)
+    {
         std::cerr << "❌ Server error: " << e.what() << std::endl;
         return EXIT_FAILURE;
     }
